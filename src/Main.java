095f2074cf5d52844503e7a1dd7027983ad88369
Marco Abbondanza <107813503+Marco9811@users.noreply.github.com>
2023-01-26 19:54:21 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] carBrands = { "BMW", "Mercedes", "Fiat", "Ferrari", "Toyota"};
+        int[] primeNumbers = {1,2,3,4,5,6};
+
+
+        System.out.println(carBrands.length);
+        System.out.println(carBrands[3]);
+
+        System.out.println(primeNumbers);
+        }
+    }
+
